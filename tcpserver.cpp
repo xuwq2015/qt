1d7e78bcadaebc56044e2aa10990adb181dda9dc
xuwq2015 <xuwq2015@163.com>
2015-12-11 15:25:32 +0800

    创建tcp服务，并读取传输数据

---
+++ tcpserver.cpp
@@ -0,0 +1,28 @@
+﻿/*
+    此类用于创建一个新的tcp服务器，通过参数tcpPort指定的端口进行监听；
+    在一个新的tcp连接进入后读取其中的传入的数据
+*/
+#include "tcpserver.h"
+
+TcpServer::TcpServer(int tcpPort, QObject *parent) : QObject(parent){
+    tcpService = new QTcpServer(this);  //创建tcp连接
+
+    tcpService->listen(QHostAddress::Any,tcpPort);  //监听tcp连接
+
+    /* 连接信号，当新的tcp连接进入后tcpServer发送此信号 */
+    connect(tcpService, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
+}
+
+/* 获取新的tcp连接的套接字 */
+void TcpServer::onNewConnection() {
+    tcpSocket = tcpService->nextPendingConnection();
+
+    /* 当次连接有数据传进来后会发送readyRead信号 */
+    connect(this->tcpSocket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
+}
+
+/* 开始读取tcp连接传进来的数据 */
+void TcpServer::onReadyRead() {
+    readData = tcpSocket->readAll();
+}
+
---
+++ tcpserver.h
@@ -0,0 +1,34 @@
+﻿/*
+    此类用于创建一个新的tcp服务器，通过参数tcpPort指定的端口进行监听；
+    在一个新的tcp连接进入后读取其中的传入的数据
+*/
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QByteArray>
+
+class TcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TcpServer(int tcpPort, QObject *parent = 0);
+
+public:
+    QTcpServer *tcpService;
+    QTcpSocket *tcpSocket;
+    QByteArray readData;    //从tcp连接中读取到的数据
+
+signals:
+
+public slots:
+    void onNewConnection();     //有新的tcp连接请求
+    void onReadyRead();         //有新的数据传入
+signals:
+
+public slots:
+};
+
+#endif // TCPSERVER_H
