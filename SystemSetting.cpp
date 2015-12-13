7059377989d8abc88270ab39ed66eeb909a03b50
xuwq2015 <xuwq2015@163.com>
2015-12-13 17:11:33 +0800

    配置文件INI的操作

---
+++ SystemSetting.cpp
@@ -0,0 +1,177 @@
+﻿/************************************************************
+  Copyright (C), 2014-2015, Everyoo Tech. Co., Ltd.
+  FileName: test.cpp
+  Author:        Version :          Date:
+  Description:     // 模块描述
+  Version:         // 版本信息
+  History:         // 历史修改记录
+      <author>  <time>   <version >   <desc>
+      David    2015/1/12     1.0     build this moudle
+***********************************************************/
+
+#include "SystemSetting.h"
+
+SystemSetting::SystemSetting(QObject *parent) : QObject(parent) {
+    saveUserNameStatus = false;
+    saveUserPasswordStatus = false;
+
+    QDir dir;
+    if(dir.exists("./configuration/")) {
+        //存在配置文件
+        systemSetting = new QSettings("./configuration/systemSetting.ini", QSettings::IniFormat);
+    } else {
+        //初始化配置文件
+        dir.mkpath("./configuration/");
+        QSettings settings("./configuration/systemSetting.ini", QSettings::IniFormat); // 当前目录的INI文件
+        settings.setValue("uplinkPort", 9901);
+        settings.setValue("downlinkPort", 9900);
+        settings.setValue("databaseType", "QMYSQL");
+        settings.setValue("hostName", "192.168.100.166");
+        settings.setValue("databasePort", 3306);
+        settings.setValue("databaseName", "woms");
+        settings.setValue("databaseUser", "root");
+        settings.setValue("databasePasswd", "everyoo");
+        systemSetting = new QSettings("./configuration/systemSetting.ini", QSettings::IniFormat);
+    }
+}
+
+SystemSetting::~SystemSetting() {
+
+}
+
+QString SystemSetting::readSettings(QString parameters) {
+    //读取系统设置
+    return systemSetting->value(parameters).toString();
+}
+
+void SystemSetting::writeSettings(QString parameters, QString value) {
+    //保存或修改系统设置
+    systemSetting->setValue(parameters, value);
+}
+
+
+void SystemSetting::modifyUserSetting(QString user, QString key, QString value) {
+    //修改用户配置或添加用户设置
+    QString userFile("./configuration/");
+    userFile.append(user);
+    userFile.append(".ini");
+
+    QSettings settings(userFile, QSettings::IniFormat);
+    settings.setValue(key, value);
+}
+
+void SystemSetting::initUserSetting(QString user) {
+    //初始化用户设置
+    QString userFile("./configuration/");
+    userFile.append(user);
+    userFile.append(".ini");
+
+    QSettings settings(userFile, QSettings::IniFormat);
+    settings.setValue("userName", user);
+    settings.setValue("savePassword", "0");
+}
+
+void SystemSetting::deleteUserSetting(QString user) {
+    //删除用户设置
+    QString userFile("./configuration/");
+    userFile.append(user);
+    userFile.append(".ini");
+
+    QDir qdir;
+    qdir.remove(userFile);
+}
+
+void SystemSetting::getSaveUser() {
+    QDir dir("./configuration/");
+    dir.setFilter(QDir::Files | QDir::NoSymLinks);
+    QFileInfoList fileInfoList = dir.entryInfoList();
+    if(!fileInfoList.isEmpty()) {
+        for(int i = 0; i < fileInfoList.size() - 1 ; i++) {
+            QFileInfo fileName(fileInfoList.at(i));
+            if(fileName.fileName() != "systemSetting.ini") {
+                setAddComboBoxItem(fileName.baseName());
+            }
+        }
+    } else {
+        setAddComboBoxItem("");
+    }
+
+    emit addComplete();
+}
+
+bool SystemSetting::getSavePassword(QString user) {
+    //判断是否已保存密码,若已保存密码则返回true,否则返回false
+    QString userFile("./configuration/");
+    userFile.append(user);
+    userFile.append(".ini");
+
+    QSettings setting(userFile, QSettings::IniFormat);
+    if(setting.value("savePassword").toInt())
+        return true;
+    else
+        return false;
+}
+
+QString SystemSetting::getUserSetting(QString user, QString key) {
+    QString userFile("./configuration/");
+    userFile.append(user);
+    userFile.append(".ini");
+
+    QSettings setting(userFile, QSettings::IniFormat);
+    return setting.value(key).toString();
+}
+
+
+bool SystemSetting::saveUserName() {
+    return saveUserNameStatus;
+}
+
+void SystemSetting::setSaveUserName(bool status) {
+    saveUserNameStatus = status;
+}
+
+bool SystemSetting::saveUserPassword() {
+    return saveUserPasswordStatus;
+}
+
+void SystemSetting::setSaveUserPassword(bool status) {
+    saveUserPasswordStatus = status;
+}
+
+QString SystemSetting::loginUserName() {
+    return loginUser;
+}
+
+void SystemSetting::setLoginUserName(QString user) {
+    loginUser = user;
+}
+
+QString SystemSetting::addComboBoxItem() {
+    return addComboBox;
+}
+
+void SystemSetting::setAddComboBoxItem(QString item) {
+    addComboBox = item;
+    emit addComboBoxItemChanged();
+}
+
+
+void SystemSetting::saveLoginSetting() {
+    if (saveUserName()) {
+        if (saveUserPassword()) {
+            modifyUserSetting(loginUserName(), "savePassword", "1");
+        } else {
+            initUserSetting(loginUserName());
+        }
+    } else {
+        QString fileName("./configuration/");
+        fileName.append(loginUserName());
+        fileName.append(".ini");
+        QFileInfo fileInfo(fileName);
+        if (fileInfo.exists()) {
+            QDir qdir;
+            qdir.remove(fileName);
+        }
+    }
+}
+
---
+++ SystemSetting.h
@@ -0,0 +1,78 @@
+﻿#ifndef _SYSTEM_SETTING_H_
+#define _SYSTEM_SETTING_H_
+
+#include <QObject>
+#include <QSettings>
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
+#include "Utils.h"
+
+class SystemSetting : public QObject {
+    Q_OBJECT
+
+    /*
+     * 宏Q_PROPERTY用来向qml中注册一个变量，使用此宏可在qml中直接调用此变量。
+     * Q_PROPERTY(type name READ name WRITE setName NOTIFY nameChanged)为宏的原型；
+     * type name: 为变量，如：QString addComboBoxItem
+     * READ name: 为读取变量的函数，在qml中使用此变量后会自动调用此函数，如：addComboBoxItem()
+     * WRITE setName: 为变量赋值函数，在qml中为变量赋值后系统自动调用此函数，如：setAddComboBoxItem();
+     * NOTIFY nameChanged: 为信号，可在qml中接收此信号，可根据实际情况发送此信号,如在调用setAddComboBoxItem()
+     * 函数后；
+     *
+     * 注意：name变量的值必须用另一个变量来存储，不可以直接用此变量来存储。
+    */
+    Q_PROPERTY(bool saveUserName READ saveUserName WRITE setSaveUserName)
+    Q_PROPERTY(bool saveUserPassword READ saveUserPassword WRITE setSaveUserPassword)
+    Q_PROPERTY(QString loginUserName READ loginUserName WRITE setLoginUserName)
+    Q_PROPERTY(QString addComboBoxItem READ addComboBoxItem WRITE setAddComboBoxItem NOTIFY addComboBoxItemChanged)
+
+public:
+    explicit SystemSetting(QObject *parent = 0);
+    ~SystemSetting();
+
+public:
+    QSettings *systemSetting;
+    bool saveUserNameStatus;
+    bool saveUserPasswordStatus;
+    QString loginUser;
+    QString addComboBox;
+
+
+public:
+
+    /*
+     * 宏Q_INVOKABLE用来向qml注册一个函数，使用此宏注册过的函数可在qml中直接调用。
+     */
+    Q_INVOKABLE QString readSettings(QString);
+    Q_INVOKABLE void writeSettings(QString, QString);
+    Q_INVOKABLE void modifyUserSetting(QString, QString, QString);  //修改或添加用户设置
+    Q_INVOKABLE void initUserSetting(QString);                      //初始化用户设置
+    Q_INVOKABLE void deleteUserSetting(QString);                    //删除用户设置
+    Q_INVOKABLE void getSaveUser();
+    Q_INVOKABLE bool getSavePassword(QString);                      //判断是否已保存密码
+    Q_INVOKABLE QString getUserSetting(QString, QString);
+    Q_INVOKABLE void saveLoginSetting();
+
+    /*对注册到qml的变量提供读取操作*/
+    bool saveUserName();
+    void setSaveUserName(bool);
+
+    bool saveUserPassword();
+    void setSaveUserPassword(bool);
+
+    QString loginUserName();
+    void setLoginUserName(QString);
+
+    QString addComboBoxItem();
+    void setAddComboBoxItem(QString);
+
+signals:
+    void addComboBoxItemChanged();
+    void addComplete();
+
+public slots:
+
+};
+
+#endif // _SYSTEM_SETTING_H_
