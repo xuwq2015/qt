9c5e1ac614a9f028f3dad88cf46de0a83426dbdb
xuwq2015 <xuwq2015@163.com>
2015-12-13 16:47:47 +0800

    数据库操作

---
+++ databaseoperate.cpp
@@ -0,0 +1,36 @@
+﻿#include "databaseoperate.h"
+
+DatabaseOperate::DatabaseOperate(dbInfo dbInfo_v, QObject *parent) : QObject(parent) {
+  db = QSqlDatabase::addDatabase(dbInfo_v.dbType);
+  db.setHostName(dbInfo_v.dbHostName);
+  db.setPort(dbInfo_v.dbPort);
+  db.setDatabaseName(dbInfo_v.dbName);
+  db.setUserName(dbInfo_v.dbUser);
+  db.setPassword(dbInfo_v.dbPasswd);
+
+  if (!(db.open())) {
+      emit connectDatabaseError(db.lastError().text());
+  }
+}
+
+DatabaseOperate::~DatabaseOperate() {
+    db.close();
+}
+
+void DatabaseOperate::addUser() {
+    QString userid = "0001";
+    QString userName = "xwq";
+    QString passwd = "123456";
+
+    QSqlQuery query;
+    /*添加用户信息*/
+    query.prepare("INSERT INTO account(id, username, passwd) VALUES (?, ?, ?)");
+
+    query.addBindValue(userid);
+    query.addBindValue(userName);
+    query.addBindValue(passwd);
+
+    if (!query.exec()) {
+        qDebug()<<query.lastError();
+    }
+}
---
+++ databaseoperate.h
@@ -0,0 +1,34 @@
+﻿#ifndef DATABASEOPERATE_H
+#define DATABASEOPERATE_H
+
+#include <QObject>
+#include <QSqlDatabase>
+#include <QString>
+
+typedef struct db_info {
+    QString dbType;         //数据库类型
+    QString dbHostName;     //数据库主机
+    QString dbPort;         //数据库服务器端口号
+    QString dbName;         //数据库名
+    QString dbUser;         //数据库用户名
+    QString dbPasswd;       //数据库密码
+}dbInfo;
+
+class DatabaseOperate : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DatabaseOperate(dbInfo dbInfo_v, QObject *parent = 0);
+    ~DatabaseOperate();
+
+    void addUser();     //添加用户信息
+
+public:
+    QSqlDatabase db;
+
+signals:
+
+public slots:
+};
+
+#endif // DATABASEOPERATE_H
